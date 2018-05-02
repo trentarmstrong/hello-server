85ec1cb3584ef4a2aa85410407ad4f68f3556024
Trent Armstrong <trent.armstrong1@gmail.com>
2018-05-02 16:31:11 -0700

    added simple logger

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 var express = require('express');
+var morgan = require('morgan');
 var app = express();
 
+var logger = morgan('combined');
 
-app.get('/', function (req, res) {
+app.use(logger);
+
+app.get('/hello', function (req, res) {
+
     var name = req.query.name || 'World';
     res.send('Hello, ' + name + '!');
 });
 
 app.listen(3000, function () {
+    
     console.log('Hello server listening on port 3000!');
 });
