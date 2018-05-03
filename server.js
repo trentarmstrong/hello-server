5755b63f011e05d79424f37989a4f97ed35d69d3
Trent Armstrong <trenta@slalom.com>
2018-05-03 10:21:55 -0700

    added some comments to test ssh key.

--- server.js
+++ server.js
@@ -6,13 +6,21 @@ var logger = morgan('combined');
 
 app.use(logger);
 
+/**
+ * GET /hello
+ */
 app.get('/hello', function (req, res) {
 
+    // Grab the name from the query parameter if it exists. Default to 'World'
     var name = req.query.name || 'World';
-    res.send('Hello, ' + name + '!');
+
+    res.send('Hello, ' + name + '! Updated with rolling update!');
 });
 
+/**
+ * Start the server.
+ */
 app.listen(3000, function () {
-    
+
     console.log('Hello server listening on port 3000!');
 });
